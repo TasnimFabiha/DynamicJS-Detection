65f128d3db6a36d621c46f35a1c6f0c65293db76
Syeda Tasnim Fabiha <tasnimfabiha022@gmail.com>
2017-10-11 08:32:10 +0600

    request for cookies added via server. not by extension

--- xssi check/xssiServer/Controllers/HomeController.cs
+++ xssi check/xssiServer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using xssiServer.Models;
@@ -9,8 +10,22 @@ namespace xssiServer.Controllers
 {
     public class HomeController : Controller
     {
+       
         public ActionResult Index(ScriptHolder scriptHolder)
         {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.foodiez.com.bd/Home");
+            request.CookieContainer = new CookieContainer();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //var cookies = new CookieContainer();
+            //cookies.Add(response.Cookies);
+
+            //request = (HttpWebRequest)WebRequest.Create(secondUrlWithParameters);
+            //request.CookieContainer = cookies;
+
+
+
             ApplicationDbContext db = new ApplicationDbContext();
             db.ScriptHolders.Add(scriptHolder);
             db.SaveChanges();
